abea6037eaa9c95f1b56f24116a641d87db76c3e
Lili Boxer <ninaliboxer@gmail.com>
2019-10-03 14:39:07 -0700

    setting up redux state

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,6 +1,8 @@
+const initialState = {
+  items: []
+};
 
-
-export default function itemReducer(state = [], action) {
+export default function itemReducer(state = initialState, action) {
   switch(action.type) {
     default:
       return state;
--- src/store.js
+++ src/store.js
@@ -4,7 +4,6 @@ import reducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
 export default createStore(
   reducer,
   composeEnhancers(
