da76e75c87f82e780d0317df8ce7ac2703fb7e3b
Lili Boxer <ninaliboxer@gmail.com>
2019-10-06 08:50:13 -0700

    dynamic dropdown menu for categories

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -2,9 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ItemForm() {
+  const categories = ['Clothing', 'Books', 'Miscellaneous', 'Sentimental'];
+
+  const createOptions = options => {
+    return options.map(option => (
+      <option key={option} value='${option}'>{option}</option>
+    ));
+  };
+  
   return (
     <form>
-      FORM
+      <fieldset>
+        <legend>Category:</legend>
+        <select>
+          <option selected></option>
+          {createOptions(categories)}
+        </select>
+      </fieldset>
+     
     </form>
   );
 
