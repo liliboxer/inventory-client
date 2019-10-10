d68dc45acbc5009f65dc1f04bde76d14013b32e5
Lili Boxer <ninaliboxer@gmail.com>
2019-10-10 10:51:47 -0700

    footer positioning and body css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import styles from './Footer.css';
 export default function Footer() {
   return (
     <footer className={styles.Footer}>
-      <a href="https://www.liliboxer.com/">© Lili Boxer 2019</a>
+      <div className={styles.wrapper}>
+        <a href="https://www.liliboxer.com/">© Lili Boxer 2019</a>
+      </div>
     </footer>
   );
 }
